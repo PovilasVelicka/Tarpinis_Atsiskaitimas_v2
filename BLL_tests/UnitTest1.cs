58f6fcc0c97f5a9cfd769727fe5fdbd4b4fd3541
Povilas Velička <100714799+PovilasVelicka@users.noreply.github.com>
2022-07-02 13:45:10 +0300

    Delete all BLL interface and classes Delete all tests Delate start Main proc.

--- BLL_tests/UnitTest1.cs
+++ BLL_tests/UnitTest1.cs
@@ -18,125 +18,125 @@ namespace BLL_tests
             };
 
 
-        [Test]
-        public void Department_CreaateCheck_Test()
-        {
+        //[Test]
+        //public void Department_CreaateCheck_Test()
+        //{
             
-            using (IServiceBLL service = new InvormationService())
-            {
-                var depoName = "CodeAcademy Klaipedos filialas";
-                var depoCity = "Klaipeda";
-                IDepartmentEntity department = service.Departments.CreateDepartment(depoName, depoCity);
+        //    using (IServiceBLL service = new InvormationService())
+        //    {
+        //        var depoName = "CodeAcademy Klaipedos filialas";
+        //        var depoCity = "Klaipeda";
+        //        IDepartmentEntity department = service.Departments.CreateDepartment(depoName, depoCity);
 
 
-                Assert.That(department.Id, Is.GreaterThan(0));
+        //        Assert.That(department.Id, Is.GreaterThan(0));
 
-                Assert.That(department.Name, Is.EqualTo(depoName));
+        //        Assert.That(department.Name, Is.EqualTo(depoName));
 
-                Assert.That(department.City, Is.EqualTo(depoCity));
+        //        Assert.That(department.City, Is.EqualTo(depoCity));
 
-                //service.Departments.DeleteDepartment(department.Id);
-            };
-        }
+        //        //service.Departments.DeleteDepartment(department.Id);
+        //    };
+        //}
 
-        [Test]
-        public void Student_CreaateAndAddToDepartment_Test()
-        {
-            using (IServiceBLL service = new InvormationService())
-            {
+        //[Test]
+        //public void Student_CreaateAndAddToDepartment_Test()
+        //{
+        //    using (IServiceBLL service = new InvormationService())
+        //    {
 
-                var depoName = "CodeAcademy Kauno filialas";
-                var depoCity = "Kaunas";
-                var firstName = "Vardenis";
-                var lastName = "Pavardenis";
-                var personalCode = "37501050001";
+        //        var depoName = "CodeAcademy Kauno filialas";
+        //        var depoCity = "Kaunas";
+        //        var firstName = "Vardenis";
+        //        var lastName = "Pavardenis";
+        //        var personalCode = "37501050001";
 
-                IDepartmentEntity department = service.Departments.CreateDepartment(depoName, depoCity);
-                IStudentEntity student = service.Students.CreateStudent(firstName, lastName, personalCode, department.Id);
+        //        IDepartmentEntity department = service.Departments.CreateDepartment(depoName, depoCity);
+        //        IStudentEntity student = service.Students.CreateStudent(firstName, lastName, personalCode, department.Id);
 
 
 
-                Assert.That(student.Id, Is.GreaterThan(0));
+        //        Assert.That(student.Id, Is.GreaterThan(0));
 
-                Assert.That(student.FirstName, Is.EqualTo(firstName));
+        //        Assert.That(student.FirstName, Is.EqualTo(firstName));
 
-                Assert.That(student.LastName, Is.EqualTo(lastName));
+        //        Assert.That(student.LastName, Is.EqualTo(lastName));
 
-                Assert.That(student.PersonalCode, Is.EqualTo(personalCode));
+        //        Assert.That(student.PersonalCode, Is.EqualTo(personalCode));
 
-                Assert.That(student.DepartmentId, Is.EqualTo(department.Id));
+        //        Assert.That(student.DepartmentId, Is.EqualTo(department.Id));
 
-                //service.Departments.DeleteDepartment(department.Id);
+        //        //service.Departments.DeleteDepartment(department.Id);
 
-            }
-        }
+        //    }
+        //}
 
-        [Test]
-        public void AddStudentToExistDepartment_Test()
-        {
-            using (IServiceBLL service = new InvormationService())
-            {
-                var depoName = "CodeAcademy Klaipedos filialas";
-                IDepartmentEntity? department = service.Departments.GetByName(depoName).FirstOrDefault();
-                if (department != null)
-                {
-                    var firstName = "Petras";
-                    var lastName = "Lakûnas";
-                    var personalCode = "36501050000";
+        //[Test]
+        //public void AddStudentToExistDepartment_Test()
+        //{
+        //    using (IServiceBLL service = new InvormationService())
+        //    {
+        //        var depoName = "CodeAcademy Klaipedos filialas";
+        //        IDepartmentEntity? department = service.Departments.GetByName(depoName).FirstOrDefault();
+        //        if (department != null)
+        //        {
+        //            var firstName = "Petras";
+        //            var lastName = "Lakûnas";
+        //            var personalCode = "36501050000";
 
-                    IStudentEntity student = service.Students.CreateStudent(firstName, lastName, personalCode, department.Id);
+        //            IStudentEntity student = service.Students.CreateStudent(firstName, lastName, personalCode, department.Id);
 
-                    Assert.That(student.Id, Is.GreaterThan(0));
+        //            Assert.That(student.Id, Is.GreaterThan(0));
 
-                    Assert.That(student.FirstName, Is.EqualTo(firstName));
+        //            Assert.That(student.FirstName, Is.EqualTo(firstName));
 
-                    Assert.That(student.LastName, Is.EqualTo(lastName));
+        //            Assert.That(student.LastName, Is.EqualTo(lastName));
 
-                    Assert.That(student.PersonalCode, Is.EqualTo(personalCode));
+        //            Assert.That(student.PersonalCode, Is.EqualTo(personalCode));
 
-                    Assert.That(student.DepartmentId, Is.EqualTo(department.Id));
+        //            Assert.That(student.DepartmentId, Is.EqualTo(department.Id));
 
-                }
+        //        }
 
-            };
-        }
+        //    };
+        //}
 
-        [Test]
-        public void CreateLectures_Test()
-        {
+        //[Test]
+        //public void CreateLectures_Test()
+        //{
             
-            using (IServiceBLL service = new InvormationService())
-            {
-                foreach (var lecture in lectureNames)
-                {
-                    service.Lectures.CreateLectrue(lecture);
-                }
-                foreach (var lecture in lectureNames)
-                {
-                    var lectures = service.Lectures.GetByName(lecture);
-                    Assert.That(lectures, Has.Count.EqualTo(1));
-                }
-            }
-        }
+        //    using (IServiceBLL service = new InvormationService())
+        //    {
+        //        foreach (var lecture in lectureNames)
+        //        {
+        //            service.Lectures.CreateLectrue(lecture);
+        //        }
+        //        foreach (var lecture in lectureNames)
+        //        {
+        //            var lectures = service.Lectures.GetByName(lecture);
+        //            Assert.That(lectures, Has.Count.EqualTo(1));
+        //        }
+        //    }
+        //}
 
-        [Test]
-        public void AssignLecturToDepartment_Test()
-        {
-            using (IServiceBLL service = new InvormationService())
-            {
-                var depoName = "CodeAcademy Klaipedos filialas";
-                var depart = service.Departments.GetByName(depoName).FirstOrDefault();
-                var lecture = service.Lectures.GetByName(lectureNames[1]).FirstOrDefault();
-                if (depart != null && lecture !=null)
-                {
-                    service.Departments.AddLecture(depart.Id, lecture);
+        //[Test]
+        //public void AssignLecturToDepartment_Test()
+        //{
+        //    using (IServiceBLL service = new InvormationService())
+        //    {
+        //        var depoName = "CodeAcademy Klaipedos filialas";
+        //        var depart = service.Departments.GetByName(depoName).FirstOrDefault();
+        //        var lecture = service.Lectures.GetByName(lectureNames[1]).FirstOrDefault();
+        //        if (depart != null && lecture !=null)
+        //        {
+        //            service.Departments.AddLecture(depart.Id, lecture);
 
-                    var depo = (Department)service.Departments.GetById(depart.Id);
-                    var lectureCount = depo.Lectures.Where(l => l.Id == lecture.Id).Count();
-                    Assert.That(lectureCount, Is.EqualTo(1));
-                }                
-            }
-        }
+        //            var depo = (Department)service.Departments.GetById(depart.Id);
+        //            var lectureCount = depo.Lectures.Where(l => l.Id == lecture.Id).Count();
+        //            Assert.That(lectureCount, Is.EqualTo(1));
+        //        }                
+        //    }
+        //}
 
         //[Test]
         //public void AssignLecturFromDepartmentToStudent_Test()
--- StudentInformationSystem/Program.cs
+++ StudentInformationSystem/Program.cs
@@ -1,95 +1,136 @@
 ﻿using StudentInformationSystem.BLL;
+using StudentInformationSystem.BLL.Models;
+IController controller = new Controller( );
 
-var service = new InvormationService( );
-Defaults(true);
-
-
-
-void Defaults (bool create)
+IStudentDto student = new StudentDto
 {
-    if (create)
-    {
-        AddDepartments( );
-        AddStudents( );
-        AddLectures( );
-        AddLecturesToStudents( );
-        service.SaveChanges( );
-    }
-}
+    FirstName = "Povilas",
+    LastName = "Velicka",
+    PersonalCode = "37501050000",
+    DepartmentName = "Klaipedos filialas",
+    DepartmenCity = "Klaipeda"
 
-void AddDepartments ( )
-{
-    for (int i = 1; i <= 10; i++)
-    {
-        service.Departments.CreateDepartment(
-            $"Mokyklos departmentas nr: {i}",
-            GetRandomCity( ));
-    }
-}
-
-void AddStudents ( )
-{
-    for (int i = 0; i < 10; i++)
-    {
-        var availableDepartments = service.Departments.GetByCity(GetRandomCity( ));
-        foreach (var depo in availableDepartments)
-        {
-            service.Students.CreateStudent(
-                firstName: $"Vardenis {new Random( ).Next(1000)}",
-                lastName: $"Pavardenis {new Random( ).Next(1000)}",
-                personalCode: GetRandomPersonalCode( ),
-                departmentId: depo.Id);
-        }
-    }
-}
-
-void AddLectures ( )
-{
-    for (int i = 0; i < 10; i++)
-    {
-        var availableDepartments = service.Departments.GetByCity(GetRandomCity( ));
-        var lectureName = $"Paskaita {DateTime.Now.AddDays(new Random( ).Next(0, 30)).ToShortDateString( )} dienos";
-
-        foreach (var depo in availableDepartments)
-        {
-            var lecture = service.Lectures.CreateLectrue(lectureName);
-
-            service.Departments.AddLecture(depo.Id, lecture);
-        }
-    }
-}
-
-void AddLecturesToStudents ( )
-{
-    var students = service.Students.GetStudentByFirstName("VarDen");
-    foreach (var student in students)
-    {
-        var depo = service.Departments.GetById(student.DepartmentId ?? 0);
-        service.Students.AddLecturesFromDepartment(student.Id, depo);
-    }
+};
+controller.AddStudent(student);
 
 
-}
 
-string GetRandomCity ( )
-{
-    string [ ] cities = new string [ ]
-    {
-            "Vilnius", "Kaunas",
-            "Klaipėda", "Šiauliai",
-            "Panevėžys", "Alytus",
-            "Marijampolė", "Mažeikiai"
-    };
 
-    return cities [ new Random( ).Next(0, cities.Count( )) ];
-}
 
-string GetRandomPersonalCode ( )
-{
-    return
-        $"{new Random( ).Next(3, 10)}" +
-        $"{new Random( ).Next(50, 99)}" +
-        $"{new Random( ).Next(1, 13)}" +
-        $"{new Random( ).Next(1, 28)}" +
-        $"{new Random( ).Next(0, 9999).ToString("0000")}";
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var service = new InvormationService( );
+//Defaults(true);
+
+
+
+//void Defaults (bool create)
+//{
+//    if (create)
+//    {
+//        AddDepartments( );
+//        AddStudents( );
+//        AddLectures( );
+//        AddLecturesToStudents( );
+//        service.SaveChanges( );
+//    }
+//}
+
+//void AddDepartments ( )
+//{
+//    for (int i = 1; i <= 10; i++)
+//    {
+//        service.Departments.CreateDepartment(
+//            $"Mokyklos departmentas nr: {i}",
+//            GetRandomCity( ));
+//    }
+//}
+
+//void AddStudents ( )
+//{
+//    for (int i = 0; i < 10; i++)
+//    {
+//        var availableDepartments = service.Departments.GetByCity(GetRandomCity( ));
+//        foreach (var depo in availableDepartments)
+//        {
+//            service.Students.CreateStudent(
+//                firstName: $"Vardenis {new Random( ).Next(1000)}",
+//                lastName: $"Pavardenis {new Random( ).Next(1000)}",
+//                personalCode: GetRandomPersonalCode( ),
+//                departmentId: depo.Id);
+//        }
+//    }
+//}
+
+//void AddLectures ( )
+//{
+//    for (int i = 0; i < 10; i++)
+//    {
+//        var availableDepartments = service.Departments.GetByCity(GetRandomCity( ));
+//        var lectureName = $"Paskaita {DateTime.Now.AddDays(new Random( ).Next(0, 30)).ToShortDateString( )} dienos";
+
+//        foreach (var depo in availableDepartments)
+//        {
+//            var lecture = service.Lectures.CreateLectrue(lectureName);
+
+//            service.Departments.AddLecture(depo.Id, lecture);
+//        }
+//    }
+//}
+
+//void AddLecturesToStudents ( )
+//{
+//    var students = service.Students.GetStudentByFirstName("VarDen");
+//    foreach (var student in students)
+//    {
+//        var depo = service.Departments.GetById(student.DepartmentId ?? 0);
+//        service.Students.AddLecturesFromDepartment(student.Id, depo);
+//    }
+
+
+//}
+
+//string GetRandomCity ( )
+//{
+//    string [ ] cities = new string [ ]
+//    {
+//            "Vilnius", "Kaunas",
+//            "Klaipėda", "Šiauliai",
+//            "Panevėžys", "Alytus",
+//            "Marijampolė", "Mažeikiai"
+//    };
+
+//    return cities [ new Random( ).Next(0, cities.Count( )) ];
+//}
+
+//string GetRandomPersonalCode ( )
+//{
+//    return
+//        $"{new Random( ).Next(3, 10)}" +
+//        $"{new Random( ).Next(50, 99)}" +
+//        $"{new Random( ).Next(1, 13)}" +
+//        $"{new Random( ).Next(1, 28)}" +
+//        $"{new Random( ).Next(0, 9999).ToString("0000")}";
+//}
